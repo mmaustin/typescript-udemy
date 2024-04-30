7449a8bef11e7531aadf917f70d1e2c8e208dbc2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-04-30 12:56:49 -0400

    typescript setup in vite

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,7 +1,8 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
+import typescriptLogo from './typescript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.ts';
+import './tutorial.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
