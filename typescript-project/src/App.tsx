1a9d452297793981ff9414ffd55be808c6acf814
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-06-05 13:17:20 -0400

    imported shadcn button component to app.tsx

--- typescript-project/src/App.tsx
+++ typescript-project/src/App.tsx
@@ -1,8 +1,14 @@
-
+import {Button} from '@/components/ui/button'
 
 const App = () => {
+  const flx = ():void =>{
+  console.log('it works'); 
+}
   return (
-    <h1 className='text-7xl font-bold text-red-400'>App</h1>
+    <div className="">
+      <h1 className='text-7xl font-bold text-red-400'>App</h1>
+      <Button variant='destructive' size='lg' onClick={flx}>Click Here</Button>
+    </div>
   )
 };
 export default App;
