a2e680a10ae844d8f88010bd928ab196179bc29f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-06-15 11:12:12 -0400

    added links to navlinks component, hidden unless lg screen

--- typescript-project/src/components/NavLinks.tsx
+++ typescript-project/src/components/NavLinks.tsx
@@ -1,9 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
-
+import { links } from '@/utils';
+import { NavLink } from 'react-router-dom';
 
 const NavLinks = () => {
   return (
-    <div>NavLinks</div>
-  )
+    <div className='hidden lg:flex justify-center items-center gap-x-4'>
+      {links.map((link) => {
+        return (
+          <NavLink
+            to={link.href}
+            className={({ isActive }) => {
+              return `capitalize font-light tracking-wide ${
+                isActive ? 'text-primary' : ''
+              }`;
+            }}
+            key={link.label}
+          >
+            {link.label}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
 }
 export default NavLinks;
