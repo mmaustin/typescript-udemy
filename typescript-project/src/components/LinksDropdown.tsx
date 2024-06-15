41a709819dee70eaaac1262a3a41dd26a0fef24d
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-06-15 10:19:32 -0400

    completed links dropdown component

--- typescript-project/src/components/LinksDropdown.tsx
+++ typescript-project/src/components/LinksDropdown.tsx
@@ -15,7 +15,35 @@ import { NavLink } from 'react-router-dom';
 
 const LinksDropdown = () => {
   return (
-    <div className="">LinksDropdown</div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='lg:hidden'>
+        <Button variant='outline' size='icon'>
+          <AlignLeft />
+
+          <span className='sr-only'>Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='w-52 lg:hidden '
+        align='start'
+        sideOffset={25}
+      >
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.label}>
+              <NavLink
+                to={link.href}
+                className={({ isActive }) => {
+                  return `capitalize w-full ${isActive ? 'text-primary' : ''}`;
+                }}
+              >
+                {link.label}
+              </NavLink>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 export default LinksDropdown;
