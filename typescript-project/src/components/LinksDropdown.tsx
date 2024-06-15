c7445778ad7228e300437a37eb3bed7c4e53ac36
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-06-15 10:06:53 -0400

    installed shadcn dropdown-menu component, added imports to links dropdown component

--- typescript-project/src/components/LinksDropdown.tsx
+++ typescript-project/src/components/LinksDropdown.tsx
@@ -1,4 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { AlignLeft } from 'lucide-react';
+import { Button } from './ui/button';
+import { links } from '@/utils';
+import { NavLink } from 'react-router-dom';
 
 
 const LinksDropdown = () => {
